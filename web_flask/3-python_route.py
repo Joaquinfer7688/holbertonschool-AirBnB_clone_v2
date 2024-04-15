3e2aebbb33900827c35932ac75f02dc0f9569d39
Joaquinfer7688 <7688@holbertonstudents.com>
2024-04-14 17:53:26 -0700

    pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,12 +7,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def HBNB():
     """
     return Hello HBNB
     """
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
@@ -21,6 +23,7 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def C(text):
     """
@@ -28,13 +31,15 @@ def C(text):
     """
     return "C " + text.replace("_", " ")
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """
     return Python by the value of the text variable
     """
-    return "Python " + text.replace("_", " ") 
+    return "Python " + text.replace("_", " ")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
